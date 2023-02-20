573c33efbc4fcf72d890a774c0746665d04f0456
etctera <yilonglei790@gmail.com>
2023-02-20 11:21:18 +0800

    giscus评论

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -44,23 +44,26 @@ const siteMetadata = {
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+      lang: 'zh-CN',
+      repo: 'AmbitionsXXXV/MyBlog',
+      repositoryId: 'R_kgDOI1Lc0w',
+      category: 'Announcements',
+      categoryId: 'DIC_kwDOI1Lc084CUUfv',
       mapping: 'pathname', // supported options: pathname, url, title
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
       metadata: '0',
       // theme example: light, dark, dark_dimmed, dark_high_contrast
       // transparent_dark, preferred_color_scheme, custom
-      theme: 'light',
+      theme: 'preferred_color_scheme',
       // theme when dark mode
       darkTheme: 'transparent_dark',
       // If the theme option above is set to 'custom`
       // please provide a link below to your custom theme css file.
       // example: https://giscus.app/themes/custom_example.css
-      themeURL: '',
+      themeURL: 'https://giscus.app/themes/custom_example.css',
+      // @ts-ignore
+      loading: 'lazy',
     },
   },
   // search: {

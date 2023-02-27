19ebfc1afe749345e68f4338a3021d15e405e26b
etctera <yilonglei790@gmail.com>
2023-02-27 21:51:06 +0800

    project页面以及静态资源更新

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -2,8 +2,8 @@ const projectsData = [
   {
     title: 'Etc blog site',
     description: `个人博客网站，发点疯，上传点笔记`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://my-blog-h1m4zpjq7-ambitionsxxxv.vercel.app/',
+    imgSrc: '/static/images/etcblog.png',
+    href: 'https://my-blog-sable-theta.vercel.app/',
   },
 ]
 
